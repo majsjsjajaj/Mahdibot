b8daeda69adac1c8acc353e02b0047e9cec808d3
majsjsjajaj <97568628+majsjsjajaj@users.noreply.github.com>
2026-02-07 05:56:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,27 @@
-from telegram.ext import Application, MessageHandler, filters
 import os
+import asyncio
+import random
+from telegram.ext import Application, CommandHandler
+from telegram import Update
 
 TOKEN = os.getenv("TOKEN")
 
-async def get_id(update, context):
-    if update.message.video:
-        await update.message.reply_text(
-            f"FILE_ID:\n{update.message.video.file_id}"
-        )
+def load_videos():
+    with open("posts.txt") as f:
+        return [x.strip() for x in f if x.strip()]
+
+async def start(update: Update, context):
+    videos = load_videos()
+    video_id = random.choice(videos)
+
+    msg = await update.message.reply_video(
+        video=video_id,
+        caption="🎬 فيديو مؤقت"
+    )
+
+    await asyncio.sleep(15)
+    await msg.delete()
 
 app = Application.builder().token(TOKEN).build()
-app.add_handler(MessageHandler(filters.VIDEO, get_id))
+app.add_handler(CommandHandler("start", start))
 app.run_polling()
