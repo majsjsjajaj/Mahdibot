563d214ec35478e00db192de276140c23e566616
majsjsjajaj <97568628+majsjsjajaj@users.noreply.github.com>
2026-02-07 05:24:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,17 @@
 import os
 import asyncio
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
 TOKEN = os.getenv("TOKEN")
 
-def load_posts():
-    with open("posts.txt", "r", encoding="utf-8") as f:
-        return f.read().splitlines()
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    posts = load_posts()
-    for post in posts:
-        msg = await update.message.reply_text(post)
-        await asyncio.sleep(15)
-        await msg.delete()
+    msg = await update.message.reply_text("هلا بيك 👋\nهذا بوت تجريبي")
+    await asyncio.sleep(15)
+    await msg.delete()
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
-app.run_polling()
+
+if __name__ == "__main__":
+    app.run_polling()
