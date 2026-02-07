2a00442d82c1d69caa4041f03b5f322073531058
majsjsjajaj <97568628+majsjsjajaj@users.noreply.github.com>
2026-02-07 07:47:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,13 +5,25 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("TOKEN")
 
-VIDEO_ID = "BAACAgEAAxkBAAIGTGmGqWWi2rLEUOcJgtzR_wTdIGN3AAJmBQACGuk4REeKcrzqTmD-OgQ"
+VIDEO_MAP = {
+    "vid1": "AAMCAQADGQECxa99aYbDTHURPoZRKBYPsnDz8QN7KI0AAgkHAAIoLDlEYaULZhPyyecBAAdtAAM6BA",
+    "vid2": "AAMCAQADGQECxa93aYbDRKECjf7xmkxb5QM_CfsWFaIAAggHAAIoLDlElkxZBYQDVFsBAAdtAAM6BA",
+    "vid3": "AAMCAQADGQECxa8eaYbDImEx9HLnmVWYc0IKnSFCNkQAAgcHAAIoLDlEXJ3qC0zRtQ4BAAdtAAM6BA",
+}
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    msg = await update.message.reply_video(
-        video=VIDEO_ID,
-        caption="🎬 فيديو مؤقت"
-    )
+    if not context.args:
+        await update.message.reply_text("❌ رابط غير صالح")
+        return
+
+    code = context.args[0]
+    video_id = VIDEO_MAP.get(code)
+
+    if not video_id:
+        await update.message.reply_text("❌ هذا الرابط غير معروف")
+        return
+
+    msg = await update.message.reply_video(video=video_id)
 
     await asyncio.sleep(15)
     await msg.delete()
